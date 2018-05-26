de72993f682c7e5b40c289ad70de74da90f3faf7
czawora <chris@Chriss-MacBook-Pro.local>
2018-05-26 17:43:35 -0400

    added firebase gradle components

--- app/src/main/java/com/bitdanfo/android/bitdanfo/LoginActivity.java
+++ app/src/main/java/com/bitdanfo/android/bitdanfo/LoginActivity.java
@@ -2,13 +2,13 @@ package com.bitdanfo.android.bitdanfo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.R
+import android.R;
 
-public class SplashActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+
     }
 }
