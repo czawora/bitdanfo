46848cebf4de68f56c769c778c22c25ead887e6d
czawora <chris@Chriss-MacBook-Pro.local>
2018-05-26 16:29:55 -0400

    first commit

---
+++ app/src/main/java/com/bitdanfo/android/bitdanfo/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.bitdanfo.android.bitdanfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.R
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
