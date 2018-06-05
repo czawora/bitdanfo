296951fde17edf36b21646b089c1c15a2b29e7d2
czawora <chris@Chriss-MacBook-Pro.local>
2018-06-05 01:16:28 -0400

    Login Activity -almost working, token not being accepted by firebase

--- app/src/main/java/com/bitdanfo/android/bitdanfo/LoginActivity.java
+++ app/src/main/java/com/bitdanfo/android/bitdanfo/LoginActivity.java
@@ -1,9 +1,13 @@
 package com.bitdanfo.android.bitdanfo;
 
+import android.*;
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.telephony.TelephonyManager;
@@ -12,15 +16,138 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.android.volley.Cache;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class LoginActivity extends Activity {
 
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_READ_PHONE_STATE = 1;
+    private static final int REQUEST_INTERNET = 2;
 
     private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+
+    private String login_url = "http://bitdonfo7463.herokuapp.com";
+
+    private RequestQueue mRequestQueue;
+
+    private boolean tokenResponseReceived = true;
+    private byte[] mToken;
+    private String mHeader_Info;
+    private String mHeader_User;
+
+
+    //subclass of Volley Request to get http response header info
+    class TokenRequest extends Request<byte[]> {
+
+        private Response.Listener<String> mListener;
+
+        public TokenRequest(String url, Response.Listener<String> listener, Response.ErrorListener elistener) {
+
+            super(Request.Method.GET, url, elistener);
+            mListener = listener;
+        }
+
+        @Override
+        protected Response<byte[]> parseNetworkResponse( NetworkResponse response ){
+
+            tokenResponseReceived = true;
+
+            //check status code
+            if (response.statusCode == 200){
+
+                mHeader_User = response.headers.get("User");
+                Log.d("mHeader_User " + mHeader_User, TAG);
+            }
+
+            mHeader_Info = response.headers.get("Info");
+            Log.d("mHeader_Info " + mHeader_Info, TAG);
+
+            mToken = response.data;
+            Log.d("token length" + mToken.length, TAG);
+
+            return Response.success(response.data, null);
+        }
+
+        @Override
+        protected void deliverResponse( byte[] response ){
+
+            //token is many bytes long
+            if ( response.length > 10 ) {
+                mListener.onResponse("good_response");
+            }
+        }
+    }
+
+
+    protected void sendTokenRequest( String deviceId ){
+
+        //ensures only 1 token request is sent at a time
+        if ( tokenResponseReceived ) {
+
+            String requestUrl = login_url + "/+" + deviceId;
+            Log.d(requestUrl, TAG);
+
+            TokenRequest tokenRequest = new TokenRequest(requestUrl,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            completeLogin();
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                        }
+                    });
+
+            tokenRequest.setTag(TAG);
+
+            Log.d("call sendTokenRequest", TAG);
+            mRequestQueue.add(tokenRequest);
+            tokenResponseReceived = false;
+
+        }
+    }
+
+    protected void completeLogin(){
+
+        Log.d("in completeLogin", TAG);
+
+        Log.v(TAG, mToken.toString());
+        mAuth.signInWithCustomToken(mToken.toString()).addOnCompleteListener(this,
+                new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCustomToken:success");
+                    mUser = mAuth.getCurrentUser();
+
+                } else {
+
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCustomToken:failure", task.getException());
+
+                }
+            }
+        });
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,25 +157,34 @@ public class LoginActivity extends Activity {
         Log.d(TAG, "get firebase instance");
         mAuth = FirebaseAuth.getInstance();
 
+        Log.d(TAG, "get newRequestQueue");
+        mRequestQueue = Volley.newRequestQueue(this);
+
         Log.d(TAG, "set login button listener");
         Button clickButton = findViewById(R.id.login_button);
         clickButton.setOnClickListener( new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
                 attemptLogin();
             }
         });
     }
 
+    @Override
+    protected void onStop () {
+        super.onStop();
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(TAG);
+        }
+    }
 
     public void login(){
 
         Log.d(TAG, "login: get deviceID");
 
         TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        String deviceId = telephonyManager.getDeviceId();
+        String deviceId = telephonyManager.getImei();
 
         if (deviceId == null){
             Toast.makeText(LoginActivity.this, "Retrieval of device ID failed", Toast.LENGTH_LONG).show();
@@ -56,11 +192,14 @@ public class LoginActivity extends Activity {
         }
 
         Log.d(TAG, "deviceID retrieved: " + deviceId);
-
+        sendTokenRequest(deviceId);
 
     }
 
     public void attemptLogin(){
+
+        boolean has_phoneStatePermission = false;
+        boolean has_internetPermission = false;
 
         Log.d(TAG, "attemptLogin: check for READ_PHONE_STATE permissions");
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_PHONE_STATE)
@@ -75,6 +214,31 @@ public class LoginActivity extends Activity {
                     REQUEST_READ_PHONE_STATE);
         } else {
 
+            has_phoneStatePermission = true;
+            Log.d(TAG, "attemptLogin: READ_PHONE_STATE permissions already granted");
+
+        }
+
+        Log.d(TAG, "attemptLogin: check for INTERNET permissions");
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // ask for permission
+
+            Log.d(TAG, "attemptLogin: requesting INTERNET permissions");
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.INTERNET},
+                    REQUEST_INTERNET);
+        } else {
+
+            has_internetPermission = true;
+            Log.d(TAG, "attemptLogin: INTERNET permissions already granted");
+
+        }
+
+        //once both permissions are granted, then login
+        if (has_internetPermission && has_phoneStatePermission) {
             login();
         }
 
@@ -93,14 +257,27 @@ public class LoginActivity extends Activity {
                     // contacts-related task you need to do.
                     Log.d(TAG, "onRequestPermissionsResult: READ_PHONE_STATE permissions received");
 
-                    login();
 
                 } else {
 
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
                 }
-                return;
+            }
+            case REQUEST_INTERNET: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Log.d(TAG, "onRequestPermissionsResult: INTERNET permissions received");
+
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
             }
 
             // other 'case' lines to check for other
